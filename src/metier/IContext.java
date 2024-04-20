c1029717eb04adff150bd3ee01d046d6d0979a22
Bilalsajai1 <contact.bilal02@gmail.com>
2024-04-20 18:15:28 +0100

    V1

---
+++ src/metier/IContext.java
@@ -0,0 +1,11 @@
+package metier;
+
+import dao.Bean;
+
+public interface IContext {
+    Object getBean(String id);
+    Object beanFactory(Bean bean) throws InstantiationException, IllegalAccessException, ClassNotFoundException;
+    void byField(Class className, Object object, String name, String ref);
+    void byContructor(Class className, Object object, String name, String ref);
+    void bySetter(Class className, Object object, String name, String ref);
+}
